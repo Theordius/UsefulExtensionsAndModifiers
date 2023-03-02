00d5bd70b1005548722ff7b86cf257cddbca8641
Theordius <87307483+Theordius@users.noreply.github.com>
2023-03-02 20:15:23 +0100

    Initial Commit

---
+++ UsefulExtensionsAndModifiers/UsefulExtensionsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulExtensionsAndModifiersApp.swift
+//  UsefulExtensionsAndModifiers
+//
+//  Created by Rafał Gęsior on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UsefulExtensionsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
