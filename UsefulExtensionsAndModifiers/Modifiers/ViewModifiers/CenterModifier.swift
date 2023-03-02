3490d4a63f7cc2841b6711a840cb69e3b7d6534b
Theordius <87307483+Theordius@users.noreply.github.com>
2023-03-02 20:26:38 +0100

    Basic configuration, Add Groups and fire Modifer to the projects

---
+++ UsefulExtensionsAndModifiers/Modifiers/ViewModifiers/CenterModifier.swift
@@ -0,0 +1,23 @@
+//
+//  CenterModifier.swift
+//  UsefulExtensionsAndModifiers
+//
+//  Created by Rafał Gęsior on 02/03/2023.
+//
+
+// A modifier which enebles you to center current content
+
+import SwiftUI
+
+struct CenterModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        HStack {
+            Spacer()
+            content
+            Spacer()
+        }
+    }
+}
+
+/// Use example:  You can use .modifier with any kind of view to make it centered on the screen.
+
